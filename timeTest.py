456ee2b14974c789c77c64feb67c0946248eb9c4
plerixus <elijaszaremba@gmail.com>
2019-10-20 19:51:18 +0100

    separate function for local log (need master backup before other changes)

--- timeTest.py
+++ timeTest.py
@@ -9,6 +9,17 @@ GPIO.setmode(GPIO.BCM)
 sensor = dht11.DHT11(pin=4)
 sleepTime = 300
 pinList = [14, 15, 18, 17]
+log_file = "results24h.txt"
+
+def local_log(file_name, time_stamp, temperature, humidity, lamp_state):
+	''' logs data on a local file '''
+	log_file = open(file_name,"a")
+	log_file.write("\nLast input:\t" + str(time_stamp))
+	log_file.write("\nTemperature:\t" + str(temperature))
+	log_file.write("\nHumidity:\t" + str(humidity))
+	log_file.write("\nLamp is:\t" + str(lamp_state) + "\n")
+	log_file.write("-" * 48)
+	log_file.close()
 
 for i in pinList:
 	GPIO.setup(i, GPIO.OUT)
@@ -17,27 +28,28 @@ for i in pinList:
 try:
     while True:
 		now = datetime.datetime.now().hour
-		log = open("results24h.txt","a")
 		result = sensor.read()
 		timeOffset = 0
-		while (result.is_valid() ==False):
+		while (result.is_valid() == False):
 			result = sensor.read()
 			timeOffset = timeOffset + 1
 			time.sleep(1)
-		log.write("\nLast input:\t" + str(datetime.datetime.now()))
-		log.write("\nTemperature:\t" + str(result.temperature))
-		log.write("\nHumidity:\t" + str(result.humidity))
 
 		if (now > 12):
 			GPIO.output(14, GPIO.HIGH)
-			now=datetime.datetime.now().hour
-			log.write("\nLamp is:\tON\n")
+			lamp_is = "ON"
 
 		else:
-			log.write("\nLamp is:\tOFF\n")
+			lamp_is = "OFF"
 
-		log.write("-"*48)
-		log.close()
+		local_log(
+			log_file,
+			datetime.datetime.now(),
+			result.temperature,
+			result.humidity,
+			lamp_is
+			)
+
 
 		time.sleep(sleepTime-timeOffset)
 
